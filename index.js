355ce78a86c3801878f9c3bcb61074fd465c170e
Rasul <overlord.ove@mail.ru>
2022-05-05 18:52:06 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const { mongoose } = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(require('./routes'))
+
+async function start() {
+  try {
+    await mongoose.connect(process.env.SERVER);
+    app.listen(process.env.PORT, () =>
+      console.log(`Сервер запушен на порте: ${process.env.PORT}`)
+    );
+  } catch (err) {
+    console.log(`Ошибка при запуске сервера: ${err.message}`);
+  }
+}
+
+start();
